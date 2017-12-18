8181d42881e704b172dc9a7a6059edccaa55d0db
RationalCoding <thomasmullendesign@gmail.com>
2017-12-18 18:38:04 -0500

    use Blob fallback

--- worker.js
+++ worker.js
@@ -2,6 +2,35 @@ var streams = []
 var prefix = '/browser-server/'
 var exclude = {{exclude}}
 
+function PseudoReadableStream (source) {
+  if (false && 'ReadableStream' in this) {
+    this._realObject = new ReadableStream(source)
+  } else {
+    var parts = []
+    var controller = {
+      enqueue: (data) => {
+        parts.push(data)
+        source.pull(controller)
+      },
+      close: () => {
+        this._realObject = new Blob(parts)
+        if (this._callback) {
+          this._callback(this._realObject)
+        }
+      }
+    }
+    source.pull(controller)
+  }
+
+  this.getRealObject = (cb) => {
+    if (this._realObject) {
+      cb(this._realObject)
+    } else {
+      this._callback = cb
+    }
+  }
+}
+
 self.addEventListener('message', function (e) {
   if (e.data.id === -1 && e.data.prefix) {
     prefix = e.data.prefix
@@ -50,7 +79,7 @@ self.addEventListener('fetch', function (e) {
           }
 
           req.started = true
-          rs = new ReadableStream({
+          rs = new PseudoReadableStream({
             pull: function (c) {
               if (pulling) return
               pulling = true
@@ -65,7 +94,9 @@ self.addEventListener('fetch', function (e) {
             }
           })
 
-          resolve(new Response(rs, {status: data.status, headers: data.headers, statusText: data.statusText}))
+          rs.getRealObject((realObject) => {
+            resolve(new Response(realObject, {status: data.status, headers: data.headers, statusText: data.statusText}))
+          })
         },
         ondata: function (data) {
           pulling = false
